07955ca35c735da3d1389b1c68bbe4eb2bda7250
Jan Sch <jan@yofu.org>
2017-12-13 21:58:17 +0100

    some layer stuff

--- keyboards/iris/keymaps/neo2/keymap.c
+++ keyboards/iris/keymaps/neo2/keymap.c
@@ -8,29 +8,139 @@
 extern keymap_config_t keymap_config;
 
 #define _NEO2 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+#define _LAYER2 1
+#define _LAYER3 2
+#define _LAYER4 16
 
 enum custom_keycodes {
   NEO2 = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
+  MOD3,
+  MOD4,
 };
 
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
 
-#define KC_LOWR LOWER
-#define KC_RASE RAISE
-#define KC_RST RESET
-#define KC_BL_S BL_STEP
+#define KC_MOD3 MOD3
+#define KC_MOD4 MOD4
 
 #define KC_AE KC_QUOT
 #define KC_OE KC_SCLN
 #define KC_UE KC_LBRC
 #define KC_SS KC_MINS
+
+
+#define KC__SL DE_SLSH
+#define KC__BSL DE_BSLS
+#define KC__LPR LSFT(KC_8)
+#define KC__RPR LSFT(KC_9)
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_NEO2] = KC_KEYMAP(
+  //,------+------+------+------+------+------.                     ,------+------+------+------+------+------.
+      ESC  ,  1   ,  2   ,  3   ,  4   ,  5   ,                        6   ,  7   ,  8   ,  9   ,  0   , BSPC ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+      TAB  ,  X   ,  V   ,  L   ,  C   ,  W   ,                        K   ,  H   ,  G   ,  F   ,  Q   , SS   ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+      MOD3 ,  U   ,  I   ,  A   ,  E   ,  O   ,                        S   ,  N   ,  R   ,  T   ,  D   ,  Z   ,
+  //|------+------+------+------+------+------+------.       ,------|------+------+------+------+------+------|
+      LSFT , UE   , OE   , AE   ,  P   ,  Y   , MOD4  ,        DEL  ,  B   ,  M   , COMM , DOT  ,  J   , RSFT ,
+  //`------+------+------+------+------+------+------/       \------+------+------+------+------+------+------'
+                                  LCTL , SPC  , LALT,           LGUI, ENT  , MOD4
+  //                            `------+------+----'          `-----+------+------'
+  ),
+
+
+  // differences in shifted layer
+  [_LAYER2] = KC_KEYMAP(
+  //,------+------+------+------+------+------.                     ,------+------+------+------+------+------.
+           ,      ,      ,      ,      ,      ,                            ,      ,      ,      ,      ,      ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+           ,      ,      ,      ,      ,      ,                            ,      ,      ,      ,      ,      ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+           ,      ,      ,      ,      ,      ,                            ,      ,      ,      ,      ,      ,
+  //|------+------+------+------+------+------+------.       ,------|------+------+------+------+------+------|
+           ,      ,      ,      ,      ,      ,      ,              ,      ,      ,      ,      ,      ,      ,
+  //`------+------+------+------+------+------+------/       \------+------+------+------+------+------+------'
+                                       ,      ,     ,               ,      ,  
+  //                            `------+------+----'          `-----+------+------'
+  ),
+
+  [_LAYER3] = KC_KEYMAP(
+  //,------+------+------+------+------+------.                     ,------+------+------+------+------+------.
+           ,      ,      ,      ,      ,      ,                            ,      ,      ,      ,      ,      ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+           ,      ,      ,      ,      ,      ,                            ,      ,      ,      ,      ,      ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+           , _BSL , _SL  ,      ,      ,      ,                            , _LPR , _RPR ,      ,      ,      ,
+  //|------+------+------+------+------+------+------.       ,------|------+------+------+------+------+------|
+           ,      ,      ,      ,      ,      ,      ,              ,      ,      ,      ,      ,      ,      ,
+  //`------+------+------+------+------+------+------/       \------+------+------+------+------+------+------'
+                                       ,      ,     ,               ,      ,  
+  //                            `------+------+----'          `-----+------+------'
+  ),
+
+
+  [_LAYER4] = KC_KEYMAP(
+  //,------+------+------+------+------+------.                     ,------+------+------+------+------+------.
+           ,      ,      ,      ,      ,      ,                            ,      ,      ,      ,      ,      ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+           , PGUP , BSPC ,  UP  , DEL  , PGDN ,                            ,  7   ,  8   ,  9   ,      ,      ,
+  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
+           , HOME , LEFT , DOWN , RIGHT,  END ,                            ,  4   ,  5   ,  6   ,      ,      ,
+  //|------+------+------+------+------+------+------.       ,------|------+------+------+------+------+------|
+           ,      ,      ,      , ENT  ,      ,      ,              ,      ,  1   ,  2   ,  3   ,      ,      ,
+  //`------+------+------+------+------+------+------/       \------+------+------+------+------+------+------'
+                                       ,      ,     ,               ,  0   ,  
+  //                            `------+------+----'          `-----+------+------'
+  )
+};
+#ifdef AUDIO_ENABLE
+float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
+#endif
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_LSFT:
+      if (record->event.pressed) {
+        layer_on(_LAYER2);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LAYER2);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return true;
+      break;
+    case MOD3:
+      if (record->event.pressed) {
+        layer_on(_LAYER3);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LAYER3);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case MOD4:
+      if (record->event.pressed) {
+        layer_on(_LAYER4);
+      } else {
+        layer_off(_LAYER4);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
 
 /*
 bool override_key(keyrecord_t* record, uint16_t normal, uint16_t shifted) {
@@ -89,115 +199,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 */
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [_NEO2] = KC_KEYMAP(
-  //,------+------+------+------+------+------.                     ,------+------+------+------+------+------.
-      ESC  ,  1   ,  2   ,  3   ,  4   ,  5   ,                        6   ,  7   ,  8   ,  9   ,  0   , BSPC ,
-  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
-      TAB  ,  X   ,  V   ,  L   ,  C   ,  W   ,                        K   ,  H   ,  G   ,  F   ,  Q   , SS   ,
-  //|------+------+------+------+------+------|                     |------+------+------+------+------+------|
-      RASE ,  U   ,  I   ,  A   ,  E   ,  O   ,                        S   ,  N   ,  R   ,  T   ,  D   ,  Y   ,
-  //|------+------+------+------+------+------+------.       ,------|------+------+------+------+------+------|
-      LSFT , UE   , OE   , AE   ,  P   ,  Z   , SPC  ,         ENT  ,  B   ,  M   , COMM , DOT  ,  J   , RSFT ,
-  //`------+------+------+------+------+------+------/       \------+------+------+------+------+------+------'
-                                  LCTL , SPC  , SPC ,          ENT  , ENT  , LALT
-  //                            `------+------+----'          `-----+------+------'
-  ),
-
-  [_LOWER] = KC_KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     RST , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     DEL ,    ,LEFT,RGHT, UP ,LBRC,               RBRC, P4 , P5 , P6 ,PLUS,PIPE,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     BL_S,    ,    ,    ,DOWN,LCBR,LPRN,     RPRN,RCBR, P1 , P2 , P3 ,MINS,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,DEL ,         DEL ,    , P0
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_RAISE] = KC_KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,MPRV,MNXT,VOLU,PGUP,UNDS,               EQL ,HOME,    ,    ,    ,BSLS,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     MUTE,MSTP,MPLY,VOLD,PGDN,MINS,    ,         ,PLUS,END ,    ,    ,    ,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,    ,             ,    ,
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_ADJUST] = KEYMAP(
-  //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
-      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RESET  , DEBUG  , RGB_HUD, RGB_SAD, RGB_VAD, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
-      BL_STEP, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
-  //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
-                                      _______, _______, _______,                  _______, _______, _______
-  //                                `--------+--------+--------'                `--------+--------+--------'
-  )
-
-};
-
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-#endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case NEO2:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
-        persistent_default_layer_set(1UL<<_NEO2);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
