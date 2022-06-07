ff0fd5f0e423509255618d4f5bf51dd43ad43cbb
Jan Schaumlöffel <jan.schaumloeffel@userlike.com>
2022-06-07 13:33:57 +0200

    unicode fix

--- keyboards/xiudi/xd75/config.h
+++ keyboards/xiudi/xd75/config.h
@@ -136,3 +136,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+
+#define UNICODE_SELECTED_MODES UC_MAC, UC_LNX, UC_WINC
