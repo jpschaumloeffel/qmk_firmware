f14fb0e1972119f3bd7d0e71a3e528d61e6614de
Jan Schaumlöffel <jan.schaumloeffel@delphin.de>
2023-11-27 15:22:59 +0100

    remove unnecessary redefine

--- keyboards/moe___slow/breadboard/mcuconf.h
+++ keyboards/moe___slow/breadboard/mcuconf.h
@@ -2,7 +2,9 @@
 
 #include_next <mcuconf.h>
 
-// probably unnecessary ?
-#undef RP_I2C_USE_I2C1
-#define RP_I2C_USE_I2C1 TRUE
+// this is probably unnecessary
+// could use this to switch to I2C0 I guess (?)
+
+// #undef RP_I2C_USE_I2C1
+// #define RP_I2C_USE_I2C1 TRUE
 
