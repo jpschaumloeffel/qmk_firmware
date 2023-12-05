78b6ef6c666698c2a6ced71871a4654bcac5bc21
Jan Schaumlöffel <jan.schaumloeffel@delphin.de>
2023-12-05 09:53:31 +0100

    i2c working on i2c0 (GP0/GP1) but with swapped lines

--- keyboards/moe___slow/breadboard/mcuconf.h
+++ keyboards/moe___slow/breadboard/mcuconf.h
@@ -2,9 +2,8 @@
 
 #include_next <mcuconf.h>
 
-// this is probably unnecessary
-// could use this to switch to I2C0 I guess (?)
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 TRUE
 
-// #undef RP_I2C_USE_I2C1
-// #define RP_I2C_USE_I2C1 TRUE
-
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1 FALSE
