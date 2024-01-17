e089a447c83e641c2fb9d5dd18f9ce99bd84b3dd
Jan Schaumloeffel <katzetini@googlemail.com>
2024-01-17 15:15:43 +0100

    fix L/V swap

--- keyboards/moe___slow/maximoe/keymaps/neo2/keymap.c
+++ keyboards/moe___slow/maximoe/keymaps/neo2/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────┼┬────┬────┬────┬────┬────┬────┬┬────┬────┬────┬────┬────┬────┬┬────┬────┬────┬────┬────┬────┐
      * │    ││ ^  │ 1  │ 2  │ 3  │ 4  │ 5  ││ 6  │ 7  │ 8  │ 9  │ 0  │ -  ││ Bck│ *  │ /  │ -  │ +  │PgUp│
      * ├────┼┼────┼────┼────┼────┼────┼────┼┼────┼────┼────┼────┼────┼────┼┼────┼────┼────┼────┼────┼────┤
-     * │    ││ -> │ X  │ L  │ V  │ C  │ W  ││ K  │ H  │ G  │ F  │ Q  │ ß  ││ Del│ 7  │ 8  │ 9  │ %  │PgDn│
+     * │    ││ -> │ X  │ V  │ L  │ C  │ W  ││ K  │ H  │ G  │ F  │ Q  │ ß  ││ Del│ 7  │ 8  │ 9  │ %  │PgDn│
      * ├────┼┼────┼────┼────┼────┼────┼────┼┼────┼────┼────┼────┼────┼────┼┼────┼────┼────┼────┼────┼────┤
      * │    ││ M3 │ U  │ I  │ A  │ E  │ O  ││ S  │ N  │ R  │ T  │ D  │ Y  ││Home│ 4  │ 5  │ 6  │ .  │Num │
      * ├────┼┼────┼────┼────┼────┼────┼────┼┼────┼────┼────┼────┼────┼────┼┼────┼────┼────┼────┼────┼────┤
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NL1] = LAYOUT_ortho_6x19(
         KC_ESC , KC_F1  , KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______,
         _______, KC_CIRC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC, KC_PAST, KC_PSLS, KC_MINS, KC_PLUS, KC_PGUP,
-        _______, KC_TAB,  KC_X,    KC_L,    KC_V,    KC_C,    KC_W,    KC_K,    KC_H,    KC_G,    KC_F,    KC_Q,    UC_SS,   KC_DEL,  KC_P7,   KC_P8,   KC_P9,   KC_PERC, KC_PGDN,
+        _______, KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,    KC_W,    KC_K,    KC_H,    KC_G,    KC_F,    KC_Q,    UC_SS,   KC_DEL,  KC_P7,   KC_P8,   KC_P9,   KC_PERC, KC_PGDN,
         _______, KC_MOD3, KC_U,    KC_I,    KC_A,    KC_E,    KC_O,    KC_S,    KC_N,    KC_R,    KC_T,    KC_D,    KC_Y,    KC_HOME, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_NUM,
         _______, KC_LNSH, UC_UE,   UC_OE,   UC_AE,   KC_P,    KC_Z,    KC_B,    KC_M,    KC_COMM, KC_DOT,  KC_J,    KC_RNSH, KC_END,  KC_P1,   KC_P2,   KC_P3,   KC_UP,   KC_PENT,
         _______, KC_LCTL, MO(_FN), KC_LALT, KC_MOD4, KC_LGUI, KC_SPC,  KC_ENT,  KC_MOD4, KC_RALT, _______, MO(_FN), KC_MOD3, _______, KC_P0,   KC_COMM, KC_LEFT, KC_DOWN, KC_RGHT
