d5b5f9c8d9b9236372088528e1a80de52c35f190
Jan Schaumlöffel <jan@yofu.org>
2025-03-01 19:02:53 +0100

    map numlock

--- keyboards/moe___slow/maximoe/keymaps/neo2/keymap.c
+++ keyboards/moe___slow/maximoe/keymaps/neo2/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_NL1] = LAYOUT_ortho_6x19(
-        KC_ESC , KC_F1  , KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______,
+        KC_ESC , KC_F1  , KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUM_LOCK, _______, _______, _______, _______, _______,
         _______, KC_CIRC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC, KC_PAST, KC_PSLS, KC_MINS, KC_PLUS, KC_PGUP,
         _______, KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,    KC_W,    KC_K,    KC_H,    KC_G,    KC_F,    KC_Q,    UC_SS,   KC_DEL,  KC_P7,   KC_P8,   KC_P9,   KC_PERC, KC_PGDN,
         _______, KC_MOD3, KC_U,    KC_I,    KC_A,    KC_E,    KC_O,    KC_S,    KC_N,    KC_R,    KC_T,    KC_D,    KC_Y,    KC_HOME, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_NUM,
